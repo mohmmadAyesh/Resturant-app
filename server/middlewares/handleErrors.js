932d192024ec4cb63551166eb6a7fb8fe271ffb2
mohmmad ayesh <73184801+mohmmadAyesh@users.noreply.github.com>
2024-06-23 00:21:55 +0300

    adding a middleware for handling errors in sending requests

--- server/middlewares/handleErrors.js
+++ server/middlewares/handleErrors.js
@@ -1,5 +1,7 @@
 const handleErrors=(err,req,res,next) => {
     const statusCode = err.statusCode || 500;
+    console.log(err.stack);
+    console.log('error handled');
     res.status(statusCode).json({ success:err.success || false,message:err.message || 'Internal Server Error'});
 }
 module.exports=handleErrors;
