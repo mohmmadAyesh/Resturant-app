68b9c91df12d21010301a04bb59b64ff5aa58864
mohmmad ayesh <73184801+mohmmadAyesh@users.noreply.github.com>
2024-06-23 00:19:51 +0300

    initializing mongo database

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose');
-const connectDB =  async () => {try{await mongoose.connect('mongodb://127.0.0.1:27017/resturant'); console.log('mongo connected')}catch(err){console.log(err);}}
+require('dotenv').config();
+const connectDB =  async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('mongo connected');
+    } catch(err) {
+        console.log(err);
+    }
+}
 module.exports = connectDB;
