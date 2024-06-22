49183f8650c3ffab4a1c94622bffef7ebfc2c1b9
mohmmad ayesh <73184801+mohmmadAyesh@users.noreply.github.com>
2024-06-23 00:23:01 +0300

    adding menu route  to handle adding items and placing orders

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -1,9 +1,10 @@
 const express=require('express');
+const AuthMiddleware=require('../middlewares/auth.js');
 const {
     getMenu,
     addMenuItem
 }=require('../controllers/menu.controller');
 const router=express.Router();
 router.get('/',getMenu);
-router.post('/add',addMenuItem);
+router.post('/add',AuthMiddleware,addMenuItem);
 module.exports=router;
