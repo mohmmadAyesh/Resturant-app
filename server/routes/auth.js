1c1bbccc57a5fce69e5cf062bb83c00d58ab5c84
mohmmad ayesh <73184801+mohmmadAyesh@users.noreply.github.com>
2024-06-23 00:22:23 +0300

    adding login and signup routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,13 @@
 const express=require('express');
+const AuthMiddleware=require('../middlewares/auth.js');
 const {
     signup,
-    login
+    login,
+    getAuthUser
 }=require('../controllers/auth.controller.js');
+const auth = require('../middlewares/auth.js');
 const router=express.Router();
 router.post('/signup',signup);
 router.post('/login',login);
+router.get('/',AuthMiddleware,getAuthUser);
 module.exports=router;
